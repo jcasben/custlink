fe9ec8b5611e353a562029dc8171cf2d54910f16
jcasben <jcasben02@gmail.com>
2024-04-20 12:04:38 +0200

    Update: added new methods in User repository

--- src/main/java/dev/jcasben/custlink/user/repository/UserRepository.java
+++ src/main/java/dev/jcasben/custlink/user/repository/UserRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByUsername(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsUserByEmail(String email);
+    boolean existsUserByUsername(String username);
+    void deleteByUsername(String username);
 }
