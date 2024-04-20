ce670416b3e16297287df88b784ac675fd8d7021
jcasben <jcasben02@gmail.com>
2024-04-20 12:08:07 +0200

    Update: added redirect path to unauthorized paths

--- src/main/java/dev/jcasben/custlink/config/SecurityConfig.java
+++ src/main/java/dev/jcasben/custlink/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authRequest ->
                         authRequest
+                                .requestMatchers("/{owner}/{name}").permitAll()
                                 .requestMatchers("/auth/**").permitAll()
                                 .anyRequest().authenticated()
                         )
