026fa37d55b1caa11758668182a16a5fb0035496
jcasben <jcasben02@gmail.com>
2024-04-27 20:01:25 +0200

    Feat: added prod app origin allowed

--- src/main/java/dev/jcasben/custlink/CustlinkApplication.java
+++ src/main/java/dev/jcasben/custlink/CustlinkApplication.java
@@ -20,7 +20,7 @@ public class CustlinkApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://custlink.vercel.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
